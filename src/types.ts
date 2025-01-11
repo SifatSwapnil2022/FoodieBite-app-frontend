4c0c10081aafe7e0d7f4f44c778866c34d633424
MD SIFATULLAH SHEIKH <103960510+SifatSwapnil2022@users.noreply.github.com>
2025-01-11 23:26:22 +0600

    update types and mobilenavLinks

--- src/types.ts
+++ src/types.ts
@@ -33,6 +33,32 @@ export type OrderStatus =
   | "outForDelivery"
   | "delivered";
 
+export type order = {
+  _id: string;
+  restaurant: Restaurant;
+  user: User;
+  cartItems: {
+    menuItemId: string;
+    name: string;
+    quantity: string;
+  }[];
+  deliveryDetails: {
+    name: string;
+    addressLine1: string;
+    city: string;
+    email: string;
+  };
+  totalAmount: number;
+  status: OrderStatus;
+  createdAt: string;
+  restaurantId: string;
+};
+export type orderStatus =
+  | "placed"
+  | "paid"
+  | "inProgress"
+  | "outForDelivery"
+  | "delivered";
 export type Order = {
   _id: string;
   restaurant: Restaurant;
